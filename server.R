cccd3ebcb94147d788a575d8de43dab6281eb297
taylormerry <tmerry@uw.edu>
2018-03-06 21:35:28 -0800

    adds a linear regression line to the plot

--- server.R
+++ server.R
@@ -22,9 +22,13 @@ server <- function(input, output) {
     return(q4.data)
   })  
   
+  linear.fit <- lm(q4.data$percentage.fell ~ q4.data$year)
+  
   output$q4.plot <- renderPlot({
-    p <- ggplot(q4.data) +
-      geom_point(aes(x = year, y = percentage.fell)) +
+    p <- ggplot(q4.data, mapping = aes(x = year, y = percentage.fell)) +
+      geom_point() +
+      geom_smooth(method = "lm", se = FALSE) +
+      geom_text(x = 2007, y = 7, label = paste("r =", round(cor(q4.data$year, q4.data$percentage.fell), 2)))
       labs(title = "Percentage of Meteorites Seen Falling Over Time")
     return(p)
   })
